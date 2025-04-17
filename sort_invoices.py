80eb82b2afb11d47245b24834d38b293da5007c3
Adam Huffman <coronablue3@gmail.com>
2025-04-17 15:10:33 -0500

    Create function for reading file names.

--- sort_invoices.py
+++ sort_invoices.py
@@ -12,7 +12,7 @@ def get_file_paths(source_arg, destination_arg):
         if os.path.exists(source_arg):
             source_path = os.path.abspath(source_arg)
         else:
-            raise ValueError(f"Source path '{source_arg}' not found, or inaccessible.")
+            raise ValueError(f"Error: Source path '{source_arg}' not found, or inaccessible.")
     else:
         source_path = current_dir
 
@@ -20,29 +20,49 @@ def get_file_paths(source_arg, destination_arg):
         if os.path.exists(destination_arg):
                 destination_path = os.path.abspath(destination_arg)
         else:
-            raise ValueError(f"Destination path '{destination_arg}' not found, or inaccessible.")
+            raise ValueError(f"Error: Destination path '{destination_arg}' not found, or inaccessible.")
     else:
         destination_path = current_dir
 
     
     return source_path, destination_path
 
+def get_file_names(source_path):
+    supported_extensions = [".pdf", ".docx", ".txt"]
+
+    try:
+        file_names = [
+            file for file in os.listdir(source_path)
+            if os.path.isfile(os.path.join(source_path, file))
+            and os.path.splitext(file)[1].lower() in supported_extensions
+        ]
+        return file_names
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Error: Directory '{source_path}' not found.")
+    except NotADirectoryError:
+        raise NotADirectoryError(f"Error: '{source_path}' is not a directory.")
+
 def main(args):
     try:
+        # Accept file path
         # The source will always be the first argument, and the destination will be the second
         source_arg, destination_arg = process_args(args)
         source_path, destination_path = get_file_paths(source_arg, destination_arg)
 
         print(source_path)
         print(destination_path)
+
+        # Read files
+        files = get_file_names(source_path)
+
+        print(files)
+
     except ValueError as e:
         print(e)
     
 
 
 if __name__ == "__main__":
-    # Accept file path
-
     # Read files
 
     # Parse Client Name and Date
